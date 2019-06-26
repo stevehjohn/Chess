6d53070916f100f89713d367cca7accc3e9359ba
Stevö John <stevehuwjohn@gmail.com>
2019-06-26 22:18:52 +0100

    -

--- Engine.ConsoleTests/Program.cs
+++ Engine.ConsoleTests/Program.cs
@@ -10,36 +10,31 @@ namespace Engine.ConsoleTests
     [ExcludeFromCodeCoverage]
     public static class Program
     {
-        private const int Depth = 7;
+        private const int Depth = 8;
 
         public static void Main()
         {
-            for (var j = 0; j < 2; j++)
+            for (var i = 1; i < Depth; i++)
             {
-                Console.WriteLine(j == 0 ? "Concurrent" : "\nSequential");
+                Console.WriteLine($"\nDepth: {i}\n");
 
-                for (var i = 1; i < Depth; i++)
+                var board = BoardBuilder.Build();
+                var engine = new ChessEngine(board, i, false);
+
+                var stopwatch = Stopwatch.StartNew();
+
+                engine.GetMove(Side.White);
+
+                stopwatch.Stop();
+
+                Console.WriteLine($"Elapsed: {stopwatch.ElapsedMilliseconds / 1000.0}");
+
+                for (var k = 0; k < i; k++)
                 {
-                    Console.WriteLine($"\nDepth: {i}\n");
+                    var max = engine.Depths[k].Max(m => m.TotalValue);
+                    var count = engine.Depths[k].Count(m => m.TotalValue == max);
 
-                    var board = BoardBuilder.Build();
-                    var engine = new ChessEngine(board, i, j == 0);
-
-                    var stopwatch = Stopwatch.StartNew();
-
-                    engine.GetMove(Side.White);
-
-                    stopwatch.Stop();
-
-                    Console.WriteLine($"Elapsed: {stopwatch.ElapsedMilliseconds / 1000.0}");
-
-                    for (var k = 0; k < i; k++)
-                    {
-                        var max = engine.Depths[k].Max(m => m.TotalValue);
-                        var count = engine.Depths[k].Count(m => m.TotalValue == max);
-
-                        Console.WriteLine($"Depth {k}, Max Score {max}, Count of Max Score Nodes {count}, Total Nodes: {engine.Depths[k].Count}");
-                    }
+                    Console.WriteLine($"Depth {k}, Max Score {max}, Count of Max Score Nodes {count}, Total Nodes: {engine.Depths[k].Count}");
                 }
             }
 
