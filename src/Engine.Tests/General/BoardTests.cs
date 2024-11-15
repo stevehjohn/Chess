d9787ede2eb39ea1a9b27bb49dfaa04ee3f4ad1c
Stevo John <stevehuwjohn@gmail.com>
2024-11-15 21:24:06 +0000

    WIP

--- src/Engine.Tests/General/BoardTests.cs
+++ src/Engine.Tests/General/BoardTests.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using Engine.General;
+using Engine.Pieces;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -19,5 +21,28 @@ public class BoardTests
     public void InitialiseCreatesCorrectBoardState()
     {
         _board.Initialise();
+
+        for (var file = 0; file < 8; file++)
+        {
+            var builder = new StringBuilder();
+            
+            for (var rank = 0; rank < 8; rank++)
+            {
+                var character = _board[file, rank] switch
+                {
+                    Kind.Pawn => 'P',
+                    _ => 'x'
+                };
+
+                builder.Append(character);
+            }
+
+            var line = builder.ToString();
+            
+            _outputHelper.WriteLine(line);
+            
+            // ReSharper disable once Xunit.XunitTestWithConsoleOutput - output doesn't show from dotnet test otherwise
+            Console.WriteLine(line);
+        }
     }
 }
