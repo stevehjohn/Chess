145cbac975ce8dc7f25f95701b9c9cb4f6383357
Stevo John <stevehuwjohn@gmail.com>
2024-11-16 18:15:02 +0000

    WIP

--- src/Engine/General/MoveBuilder.cs
+++ src/Engine/General/MoveBuilder.cs
@@ -6,8 +6,12 @@ public class MoveBuilder
 {
     private Colour _colour;
 
+    private int _direction;
+
     public MoveBuilder(Colour colour)
     {
         _colour = colour;
+
+        _direction = _colour == Colour.Black ? 1 : 0;
     }
 }
