42f70487ca00d18fe9bcc1ab8cf5e19b03049bdb
Stevo John <stevehuwjohn@gmail.com>
2024-11-17 21:12:04 +0000

    WIP

--- src/Engine.Tests/Pieces/KingTests.cs
+++ src/Engine.Tests/Pieces/KingTests.cs
@@ -18,4 +18,19 @@ public class KingTests : PieceTestBase
 
         AssertAllExpectedMovesAreReturned(king, rank, file, expected);
     }
+    
+    [Theory]
+    [InlineData(0, 0, 1, 0, "0,1")]
+    [InlineData(Constants.BottomRank, Constants.RightmostFile, 7, 6, "6,7")]
+    // [InlineData(Constants.BottomRank, 0, "7,1|6,0")]
+    // [InlineData(0, Constants.RightmostFile, "0,6|1,7")]
+    // [InlineData(3, 3, "2,3|4,3|3,2|3,4")]
+    public void KingIsBlockedByOwnPieces(int rank, int file, int blockerRank, int blockerFile, string expected)
+    {
+        var king = new King(Colour.White);
+
+        Board[blockerRank, blockerFile] = new Pawn(Colour.White);
+        
+        AssertAllExpectedMovesAreReturned(king, rank, file, expected);
+    }
 }
