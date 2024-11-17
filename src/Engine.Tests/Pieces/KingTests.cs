96559b1507071323ca84e13efbc08cdb6c289658
Stevo John <stevehuwjohn@gmail.com>
2024-11-17 21:14:33 +0000

    WIP

--- src/Engine.Tests/Pieces/KingTests.cs
+++ src/Engine.Tests/Pieces/KingTests.cs
@@ -23,8 +23,8 @@ public class KingTests : PieceTestBase
     [InlineData(0, 0, 1, 0, "0,1")]
     [InlineData(Constants.BottomRank, Constants.RightmostFile, 7, 6, "6,7")]
     [InlineData(Constants.BottomRank, 0, 7, 1, "6,0")]
-    // [InlineData(0, Constants.RightmostFile, "0,6|1,7")]
-    // [InlineData(3, 3, "2,3|4,3|3,2|3,4")]
+    [InlineData(0, Constants.RightmostFile, 0, 6, "1,7")]
+    [InlineData(3, 3, 4, 3, "2,3|3,2|3,4")]
     public void KingIsBlockedByOwnPieces(int rank, int file, int blockerRank, int blockerFile, string expected)
     {
         var king = new King(Colour.White);
