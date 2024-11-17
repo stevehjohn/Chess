4aa8e32118afb9d34b4a561a92fbe9c04c292b5f
Stevo John <stevehuwjohn@gmail.com>
2024-11-17 21:04:50 +0000

    WIP

--- src/Engine.Tests/Pieces/KingTests.cs
+++ src/Engine.Tests/Pieces/KingTests.cs
@@ -10,7 +10,7 @@ public class KingTests : PieceTestBase
     [InlineData(0, 0, "1,0|0,1")]
     [InlineData(Constants.BottomRank, Constants.RightmostFile, "6,7|7,6")]
     [InlineData(Constants.BottomRank, 0, "7,1|6,0")]
-    [InlineData(0, Constants.RightmostFile, "0,1|1,0")]
+    [InlineData(0, Constants.RightmostFile, "0,6|1,7")]
     [InlineData(3, 3, "2,3|4,3|3,2|3,4")]
     public void KingReturnsCorrectMovesOnEmptyBoard(int rank, int file, string expected)
     {
