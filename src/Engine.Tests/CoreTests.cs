2e67ffc7c33b9ddb13861e5ec8667a71aa16b43b
Stevo John <stevehuwjohn@gmail.com>
2024-11-17 22:00:33 +0000

    WIP

--- src/Engine.Tests/CoreTests.cs
+++ src/Engine.Tests/CoreTests.cs
@@ -1,6 +1,15 @@
+using Engine.General;
+using Xunit;
+
 namespace Engine.Tests;
 
 public class CoreTests
 {
-    
+    private Board _board = new Board();
+
+    [Theory]
+    [InlineData(1, 20)]
+    public void MovesPerPly(int plies, int expectedCombinations)
+    {
+    }
 }
