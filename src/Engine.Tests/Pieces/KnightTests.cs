1e3ad3decab4f5ed05517fe02575e1be96c9cb87
Stevo John <stevehuwjohn@gmail.com>
2024-11-17 10:53:21 +0000

    WIP

--- src/Engine.Tests/Pieces/KnightTests.cs
+++ src/Engine.Tests/Pieces/KnightTests.cs
@@ -1,3 +1,4 @@
+using Engine.General;
 using Engine.Pieces;
 using Xunit;
 
@@ -11,6 +12,20 @@ public class KnightTests : PieceTestBase
     public void KnightReturnsCorrectMovesOnEmptyBoard(int rank, int file, string expected)
     {
         var knight = new Knight(Colour.White);
+
+        AssertAllExpectedMovesAreReturned(knight, rank, file, expected);
+    }
+
+    [Theory]
+    [InlineData(Constants.WhiteHomeRank, Constants.LeftKnightFile, "5,0|5,2")]
+    [InlineData(Constants.WhiteHomeRank, Constants.RightKnightFile, "5,5|5,7")]
+    [InlineData(Constants.BlackHomeRank, Constants.LeftKnightFile, "2,0|2,2")]
+    [InlineData(Constants.BlackHomeRank, Constants.RightKnightFile, "2,5|2,7")]
+    public void KnightReturnsCorrectStartingMoves(int rank, int file, string expected)
+    {
+        Board.InitialisePieces();
+
+        var knight = Board[rank, file];
 
         AssertAllExpectedMovesAreReturned(knight, rank, file, expected);
     }
