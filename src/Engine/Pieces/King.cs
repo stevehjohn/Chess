34fee3bbe0da9f83467c02c49148ec4bf0ed3454
Stevo John <stevehuwjohn@gmail.com>
2024-11-17 20:59:59 +0000

    WIP

--- src/Engine/Pieces/King.cs
+++ src/Engine/Pieces/King.cs
@@ -1,7 +1,17 @@
+using Engine.Extensions;
+
 namespace Engine.Pieces;
 
 public class King : Piece
 {
+    private static readonly List<(int Forwards, int Right)> Moves =
+    [
+        (1, 0),
+        (-1, 0),
+        (0, 1),
+        (0, -1)
+    ];
+
     public override Kind Kind => Kind.King;
 
     public King(Colour colour) : base(colour)
@@ -10,6 +20,19 @@ public class King : Piece
 
     protected override IEnumerable<int> GetMoves()
     {
-        return [];
+        foreach (var move in Moves)
+        {
+            var cell = (Rank + move.Forwards * Direction, File + move.Right).GetCellIndex();
+
+            if (cell < 0)
+            {
+                continue;
+            }
+
+            if (Board.IsEmpty(cell))
+            {
+                yield return cell;
+            }
+        }
     }
 }
