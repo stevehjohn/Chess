1dcf84c58fde76489d5deee37d698b5c2ca213cc
Stevo John <stevehuwjohn@gmail.com>
2024-11-17 15:20:58 +0000

    WIP

--- src/Engine.Tests/Pieces/RookTests.cs
+++ src/Engine.Tests/Pieces/RookTests.cs
@@ -1,3 +1,4 @@
+using Engine.General;
 using Engine.Pieces;
 using Xunit;
 
@@ -7,14 +8,14 @@ public class RookTests : PieceTestBase
 {
     [Theory]
     [InlineData(0, 0, "1,0|2,0|3,0|4,0|5,0|6,0|7,0|0,1|0,2|0,3|0,4|0,5|0,6|0,7")]
-    // [InlineData(Constants.BottomRank, Constants.RightmostFile, "6,6|5,5|4,4|3,3|2,2|1,1|0,0")]
-    // [InlineData(Constants.BottomRank, 0, "6,1|5,2|4,3|3,4|2,5|1,6|0,7")]
-    // [InlineData(0, Constants.RightmostFile, "1,6|2,5|3,4|4,3|5,2|6,1|7,0")]
-    // [InlineData(3, 3, "2,2|1,1|0,0|2,4|1,5|0,6|4,2|5,1|6,0|4,4|5,5|6,6|7,7")]
+    [InlineData(Constants.BottomRank, Constants.RightmostFile, "6,7|5,7|4,7|3,7|2,7|1,7|0,7|7,6|7,5|7,4|7,3|7,2|7,1|7,0")]
+    [InlineData(Constants.BottomRank, 0, "6,0|5,0|4,0|3,0|2,0|1,0|0,0|7,1|7,2|7,3|7,4|7,5|7,6|7,7")]
+    [InlineData(0, Constants.RightmostFile, "0,6|0,5|0,4|0,3|0,2|0,1|0,0|1,7|2,7|3,7|4,7|5,7|6,7|7,7")]
+    [InlineData(3, 3, "2,3|1,3|0,3|4,3|5,3|6,3|7,3|3,2|3,1|3,0|3,4|3,5|3,6|3,7")]
     public void BishopReturnsCorrectMovesOnEmptyBoard(int rank, int file, string expected)
     {
-        var bishop = new Bishop(Colour.White);
+        var rook = new Rook(Colour.White);
 
-        AssertAllExpectedMovesAreReturned(bishop, rank, file, expected);
+        AssertAllExpectedMovesAreReturned(rook, rank, file, expected);
     }
 }
