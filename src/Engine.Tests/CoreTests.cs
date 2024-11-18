534e71a77fe723416f49ae627591e2a2498bf58c
Stevo John <stevehuwjohn@gmail.com>
2024-11-18 09:26:25 +0000

    WIP

--- src/Engine.Tests/CoreTests.cs
+++ src/Engine.Tests/CoreTests.cs
@@ -1,17 +1,31 @@
+using Engine.Pieces;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Engine.Tests;
 
 public class CoreTests
 {
-    private readonly Core _core = new();
+    private readonly ITestOutputHelper _outputHelper;
     
+    private readonly Core _core = new();
+
+    public CoreTests(ITestOutputHelper outputHelper)
+    {
+        _outputHelper = outputHelper;
+    }
+
     [Theory]
     [InlineData(3)]
     public void MovesPerPly(int depth)
     {
         _core.Initialise();
-        
-        
+
+        _core.GetMove(Colour.White, depth);
+
+        for (var i = 1; i <= depth; i++)
+        {
+            _outputHelper.WriteLine($"Depth: {i,2}    Combinations: {_core.DepthCounts[i],13:N0}    Expected: ");
+        }
     }
 }
