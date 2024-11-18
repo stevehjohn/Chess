5ad1f9970745f633b414d7f23c6d8d4017141c39
Stevo John <stevehuwjohn@gmail.com>
2024-11-18 09:34:23 +0000

    WIP

---
+++ src/Engine.ConsoleTests/EntryPoint.cs
@@ -0,0 +1,26 @@
+﻿using Engine.Pieces;
+
+namespace Engine.ConsoleTests;
+
+public static class EntryPoint
+{
+    private const int Depth = 6;
+    
+    public static void Main()
+    {
+        var core = new Core();
+        
+        core.Initialise();
+        
+        Console.Clear();
+        
+        Console.WriteLine();
+        
+        core.GetMove(Colour.White, 6);
+
+        for (var i = 1; i < Depth; i++)
+        {
+            Console.WriteLine($"  Depth: {i,2}    Combinations: {core.DepthCounts[i],13:N0}    Expected: ");
+        }
+    }
+}
