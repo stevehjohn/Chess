4d5ec7a0a199ac843e63bf6018ca15047dcf1bc6
Stevo John <stevehuwjohn@gmail.com>
2024-11-20 13:49:06 +0000

    Added expected checkmate counts

--- src/Engine.ConsoleTests/EntryPoint.cs
+++ src/Engine.ConsoleTests/EntryPoint.cs
@@ -27,46 +27,63 @@ public static class EntryPoint
         { (1, PlyOutcome.Castle), 0 },
         { (1, PlyOutcome.Promotion), 0 },
         { (1, PlyOutcome.Check), 0 },
+        { (1, PlyOutcome.CheckMate), 0 },
+        
         { (2, PlyOutcome.Capture), 0 },
         { (2, PlyOutcome.EnPassant), 0 },
         { (2, PlyOutcome.Castle), 0 },
         { (2, PlyOutcome.Promotion), 0 },
         { (2, PlyOutcome.Check), 0 },
+        { (2, PlyOutcome.CheckMate), 0 },
+        
         { (3, PlyOutcome.Capture), 34 },
         { (3, PlyOutcome.EnPassant), 0 },
         { (3, PlyOutcome.Castle), 0 },
         { (3, PlyOutcome.Promotion), 0 },
         { (3, PlyOutcome.Check), 12 },
+        { (3, PlyOutcome.CheckMate), 0 },
+        
         { (4, PlyOutcome.Capture), 1_576 },
         { (4, PlyOutcome.EnPassant), 0 },
         { (4, PlyOutcome.Castle), 0 },
         { (4, PlyOutcome.Promotion), 0 },
         { (4, PlyOutcome.Check), 469 },
+        { (4, PlyOutcome.CheckMate), 8 },
+        
         { (5, PlyOutcome.Capture), 82_719 },
         { (5, PlyOutcome.EnPassant), 258 },
         { (5, PlyOutcome.Castle), 0 },
         { (5, PlyOutcome.Promotion), 0 },
         { (5, PlyOutcome.Check), 27_351 },
+        { (5, PlyOutcome.CheckMate), 347 },
+        
         { (6, PlyOutcome.Capture), 2_812_008 },
         { (6, PlyOutcome.EnPassant), 5_248 },
         { (6, PlyOutcome.Castle), 0 },
         { (6, PlyOutcome.Promotion), 0 },
         { (6, PlyOutcome.Check), 809_099 },
+        { (6, PlyOutcome.CheckMate), 10_828 },
+        
         { (7, PlyOutcome.Capture), 108_329_926 },
         { (7, PlyOutcome.EnPassant), 319_617 },
         { (7, PlyOutcome.Castle), 883_453 },
         { (7, PlyOutcome.Promotion), 0 },
         { (7, PlyOutcome.Check), 33_103_848 },
+        { (7, PlyOutcome.CheckMate), 0435_767 },
+        
         { (8, PlyOutcome.Capture), 3_523_740_106 },
         { (8, PlyOutcome.EnPassant), 7_187_977 },
         { (8, PlyOutcome.Castle), 23_605_205 },
         { (8, PlyOutcome.Promotion), 0 },
         { (8, PlyOutcome.Check), 968_981_593 },
+        { (8, PlyOutcome.CheckMate), 9_852_036 },
+        
         { (9, PlyOutcome.Capture), 125_208_536_153 },
         { (9, PlyOutcome.EnPassant), 319_496_827 },
         { (9, PlyOutcome.Castle), 1_784_356_000 },
         { (9, PlyOutcome.Promotion), 17_334_376 },
-        { (9, PlyOutcome.Check), 36_095_901_903 }
+        { (9, PlyOutcome.Check), 36_095_901_903 },
+        { (9, PlyOutcome.CheckMate), 400_191_963 }
     };
     
     public static void Main(string[] arguments)
