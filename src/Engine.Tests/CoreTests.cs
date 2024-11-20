ef480c9174b6f590c581933d636fede49b81d0a9
Stevo John <stevehuwjohn@gmail.com>
2024-11-20 11:24:53 +0000

    Faster unit tests

--- src/Engine.Tests/CoreTests.cs
+++ src/Engine.Tests/CoreTests.cs
@@ -30,7 +30,7 @@ public class CoreTests
     }
 
     [Theory]
-    [InlineData(5)]
+    [InlineData(4)]
     public void MovesPerPly(int maxDepth)
     {
         for (var i = 1; i <= maxDepth; i++)
